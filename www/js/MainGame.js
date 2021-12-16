61f5f3edcb7f4795630636cdf470a56534a7bd6f
k19rs030 <k19rs030@st.kyusan-u.ac.jp>
2021-12-16 10:40:00 +0900

    キー入力の方法を確認キャンバス実装中

--- www/js/MainGame.js
+++ www/js/MainGame.js
@@ -1,2 +1,19 @@
 // This is a JavaScript file
-
+document.addEventListener("keypress",OnKeyPressed);
+var s=0;
+function OnKeyPressed(e)
+{
+}
+function startGame()
+{
+  // setInterval('update()',100)
+}
+function update()
+{
+  console.log(s);
+  s++;
+  if(s>20)
+  {
+    clearInterval();
+  }
+}
