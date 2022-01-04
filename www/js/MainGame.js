7a7ccde058b8dc2589803cd583c4794c45e2926c
k19rs030 <k19rs030@st.kyusan-u.ac.jp>
2022-01-04 21:15:26 +0900

    エネミークラスを削除　処理を簡略化した

--- www/js/MainGame.js
+++ www/js/MainGame.js
@@ -8,21 +8,19 @@ var px=0;
 var ex=0,ey=0;
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
-  var enemy1;
-  var enemy2;
-  var enemy3;
+  var alive=false;
   
 function OnKeyPressed(e)
 {
   if(e.key=='a')
   {
     // console.log(--px);
-    --px;
+    px=px-30;
   }
   else if(e.key=='d')
   {
     // console.log(++px);
-    ++px;
+    px=px+30;
   }
 }
 function Awake()
@@ -31,9 +29,6 @@ function Awake()
   ctx.strokeStyle="black";
   ctx.strokeRect(0,0,299,299);
   // gamePlay=setInterval('update()',100)
-  enemy1= new Enemy();
-  enemy2= new Enemy();
-  enemy3= new Enemy();
 }
 function OnPressedStartButton()
 {
@@ -48,38 +43,6 @@ function update()
   enemy1.move();
   enemy2.move();
   enemy3.move();
-}
-class Enemy
-{
-  constructor()
-  {
-    var moving=false;
-    var alive;
-    this.x=getRandomInt(0,270);
-    this.y=0;
-  }
-  move()
-  {
-    if(moving)
-    {
-      this.y+=10;
-      ctx.fillRect(this.x,this.y,30,30);
-      console.log(this.y);
-      if(this.y>300)
-      {
-        this.respawn();
-      } 
-    }
-  }
-  respawn()
-  {
-    this.x=getRandomInt(0,270);
-    this.y=0;
-  }
-}
-function spawnEnemy()
-{
-  
 }
 function getRandomInt(min, max) {
   min = Math.ceil(min);
